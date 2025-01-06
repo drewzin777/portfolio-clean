5b661042c7eb739f3adcee1be7b95f209774ef1e
Andrew Amento <surandrew777@gmail.com>
2025-01-06 11:11:56 -0800

    Trying to fix map loading problem

--- functions/getApiKey.js
+++ functions/getApiKey.js
@@ -1,6 +1,14 @@
 exports.handler = async () => {
+    if (!process.env.GOOGLE_MAPS_API_KEY) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "GOOGLE_MAPS_API_KEY is not set" }),
+        };
+    }
     return {
-        statusCode: 200, 
-        body: JSON.stringify({apiKey: process.env.GOOGLE_MAPS_API_KEY}),
+        statusCode: 200,
+        body: JSON.stringify({ apiKey: process.env.GOOGLE_MAPS_API_KEY }),
     };
 };
+
+
