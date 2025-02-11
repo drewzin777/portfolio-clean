e858c83c891908526e42fe24fff2d9d7a180605e
Andrew Amento <surandrew777@gmail.com>
2025-02-11 10:13:23 -0800

    deleted a file in Im Hungry and fixed something in JS

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-    // Smooth scrolling
-    $('nav a').on('click', function(e) {
-      e.preventDefault();
-      const target = $(this).attr('href');
+  // Smooth scrolling
+  $('nav a').on('click', function(e) {
+    e.preventDefault();
+    
+    const target = $(this).attr('href');
+    if (target.length && $(target).length) { // Ensure target exists
       $('html, body').animate({
         scrollTop: $(target).offset().top
       }, 1000);
-    });
+    }
   });
+});
+
   
